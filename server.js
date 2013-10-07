5bc15793d27e8fd42b542dabc0b7b6aea0caab90
Per Jansson <per.r.jansson@gmail.com>
2013-10-07 19:23:50 +0200

    Switched to mcc backend

--- server.js
+++ server.js
@@ -1,5 +1,40 @@
+var port = 1337;
+
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (mcc) is running..');
-}).listen(9407);
+
+var httpServer = http.createServer(function(request, response) {
+  logMessage((new Date()) + ' Received request for ' + request.url);
+  response.writeHead(200, {'Content-type': 'text/plain'});
+  response.end('MCC backend server started :)\n');
+});
+
+httpServer.listen(port, function() {
+  logMessage('HTTP server is listening on port ' + port);
+});
+
+
+var WebSocketServer = require('websocket').server;
+
+wsServer = new WebSocketServer({
+  httpServer: httpServer,
+  autoAcceptConnections: false
+});
+
+wsServer.on('request', function(request) {
+  var connection = request.accept();
+  logMessage('WebSocket connection accepted on server side using Node.js :)');
+
+  connection.on('message', function(message) {
+    var meeting = JSON.parse(message.utf8Data);
+    logMessage('Meeting ' + meeting.status + ':' + JSON.stringify(meeting, null, 4));
+  });
+  
+  connection.on('close', function(reasonCode, description) {
+    logMessage((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+  });
+
+});
+
+function logMessage(message) {
+  console.log(message);
+}
