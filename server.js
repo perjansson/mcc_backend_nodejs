ef0d5aee6a514e29b3ebff3dcbea55dceff744b7
Per Jansson <per.r.jansson@gmail.com>
2013-10-28 10:04:21 +0100

    First try of connection to couchdb. Added cradle module.

--- server.js
+++ server.js
@@ -9,26 +9,45 @@ var app = require('http').createServer(handler)
 app.listen(port);
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+  var response = '';
 
-    res.writeHead(200);
-    res.end(data);
+  var cradle = require('cradle');
+  var connection = new(cradle.Connection)('http://per_jansson:8sP50bjSk3@81.169.133.153:5984/mcc', 443, {
+      auth: { username: 'per_jansson', password: '8sP50bjSk3' }
   });
+
+  var db = connection.database('mcc');
+  db.save('nodeJsBackendWelcomeMessage', {
+      message: 'Node.js backend for Meeting Cost Calculator d-(*_*)z'
+  }, function (err, res) {
+    if (err) {
+        // Handle error
+        response += ' SAVE ERROR: Could not save record!!\n';
+    } else {
+        // Handle success
+        response += ' SUCESSFUL SAVE\n';
+    }
+    db.get('document_key', function (err, doc) {
+        response += ' DOCUMENT: ' + doc + '\n';
+        res.end(response);
+    });
+  });
+
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(response);
 }
 
 io.sockets.on('connection', function (socket) {
-  socket.on('meeting update', function (data) {
+  socket.on('meeting update request', function (data) {
     var meeting = JSON.parse(data);
     logMessage('Meeting ' + meeting.status + ':' + JSON.stringify(meeting, null, 4));
     if (meeting.id == null || meeting.id == '') {
       meeting.id = uuid.v4();
     }
-    socket.emit('message', meeting);
+
+
+
+    socket.emit('meeting update response', meeting);
   });
 });
 
