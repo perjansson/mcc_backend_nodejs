42100e70c0c90cd5e0f78b5b796705c5c1e50fee
Per Jansson <per.r.jansson@gmail.com>
2014-02-13 21:48:25 +0100

    Minor change.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 1337;
 var http = require('http');
 var app = http.createServer(function (req, http_res) {
     http_res.writeHead(200, {'Content-Type': 'text/plain'});
-    http_res.end('Node.js server up and running v-(`_´)z');
+    http_res.end('Node.js server up and running v-(*_*)z');
 });
 
 app.listen(port);
